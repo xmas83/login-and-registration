3e69a0a0c4ca013924b1b54ed84707c1c99b17e6
hamid <alijani.hamid@yahoo.com>
2022-03-09 23:36:58 +0100

    registration form created

--- src/main/java/com/techomeck/login/and/registration/controller/AppController.java
+++ src/main/java/com/techomeck/login/and/registration/controller/AppController.java
@@ -1,7 +1,9 @@
 package com.techomeck.login.and.registration.controller;
 
 
+import com.techomeck.login.and.registration.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +12,11 @@ public class AppController {
     @GetMapping("")
     public String viewHomePage(){
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showSignupForm(Model model){
+        model.addAttribute("user",new User());
+        return "signup_form";
     }
 }
