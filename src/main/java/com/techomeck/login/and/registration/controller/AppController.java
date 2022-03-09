0ed25f2bda3a100da3c47909dded5567015e6977
hamid <alijani.hamid@yahoo.com>
2022-03-09 23:59:02 +0100

    Handler for registration implemented

--- src/main/java/com/techomeck/login/and/registration/controller/AppController.java
+++ src/main/java/com/techomeck/login/and/registration/controller/AppController.java
@@ -2,21 +2,35 @@ package com.techomeck.login.and.registration.controller;
 
 
 import com.techomeck.login.and.registration.entity.User;
+import com.techomeck.login.and.registration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
 
+    @Autowired
+    private UserRepository repo;
+
+
     @GetMapping("")
-    public String viewHomePage(){
+    public String viewHomePage() {
         return "index";
     }
 
     @GetMapping("/register")
-    public String showSignupForm(Model model){
-        model.addAttribute("user",new User());
+    public String showSignupForm(Model model) {
+        model.addAttribute("user", new User());
         return "signup_form";
     }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user) {
+        repo.save(user);
+        return "register_success";
+    }
+
 }
