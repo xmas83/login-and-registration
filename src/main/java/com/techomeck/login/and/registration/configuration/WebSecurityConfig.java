2d67bef6b0b8d1f12b758085fea1be8cd0a92c4a
hamid <alijani.hamid@yahoo.com>
2022-03-11 10:53:23 +0100

    User List page implemented

--- src/main/java/com/techomeck/login/and/registration/configuration/WebSecurityConfig.java
+++ src/main/java/com/techomeck/login/and/registration/configuration/WebSecurityConfig.java
@@ -46,9 +46,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/list_users").authenticated()
-                .anyRequest().permitAll().and().formLogin().usernameParameter("email")
+        http.authorizeRequests()
+                .antMatchers("/list_users").authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin()
+                .usernameParameter("email")
                 .defaultSuccessUrl("/list_users")
-                .permitAll().and().logout().logoutSuccessUrl("/").permitAll();
+                .permitAll()
+                .and().logout()
+                .logoutSuccessUrl("/")
+                .permitAll();
     }
 }
--- src/main/java/com/techomeck/login/and/registration/controller/AppController.java
+++ src/main/java/com/techomeck/login/and/registration/controller/AppController.java
@@ -4,10 +4,13 @@ package com.techomeck.login.and.registration.controller;
 import com.techomeck.login.and.registration.entity.User;
 import com.techomeck.login.and.registration.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -29,8 +32,17 @@ public class AppController {
 
     @PostMapping("/process_register")
     public String processRegistration(User user) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encoderPassword = encoder.encode(user.getPassword());
+        user.setPassword(encoderPassword);
         repo.save(user);
         return "register_success";
     }
 
+    @GetMapping("/list_users")
+    public String ViewUsersList(Model model) {
+        List<User> listUsers = repo.findAll();
+        model.addAttribute("listUsers", listUsers);
+        return "users";
+    }
 }
